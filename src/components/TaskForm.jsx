0eb49981eee9d6be7130737e1b5e489e5ac0dad8
umair22 <mumair2262@gmail.com>
2025-09-27 16:33:46 +0500

    Handling the Form Files

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import "./TaskForm.css"
 import Tag from './Tag'
+import { useState } from "react"
 
 const TaskForm = () => {
+  const [task, setTask] = useState("");
+  const [status, setStatus] = useState("todo");
+  const handleTaskchange = e => {
+    setTask(e.target.value);
+  };
+  const handleStatuschange = e => {
+    setStatus(e.target.value);
+  };
+  console.log(task,status)
   return (
     <header className='app_header'>
       <form >
-        <input type="text" className="task_input" placeholder='Enter your task' />
+        <input type="text" className="task_input" placeholder='Enter your task'
+        onChange={handleTaskchange}  />
 
         <div className='task_form_bottom_line'>
           <div>
@@ -17,7 +28,7 @@ const TaskForm = () => {
           
           </div>
           <div>
-          <select className="task_status" >
+          <select className="task_status" onChange={handleStatuschange}>
             <option value="todo">To do</option>
             <option value="doing">Doing</option>
             <option value="done">Done</option>
